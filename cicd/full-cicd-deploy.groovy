48e031fb657e92b36d4f04cd372dbb806a2318ce
Ubuntu <ubuntu@ip-172-31-47-63.ap-southeast-1.compute.internal>
2025-09-08 02:05:51 +0000

    update

--- cicd/full-cicd-deploy.groovy
+++ cicd/full-cicd-deploy.groovy
@@ -6,7 +6,7 @@ pipeline {
     }
 
     environment {
-        DOCKER_IMAGE = "orvencasido/resume-project-ansible"
+        DOCKER_IMAGE = "orvencasido/resume-project-ansible-2"
         VERSION = "${env.BUILD_NUMBER}"
     }
 
